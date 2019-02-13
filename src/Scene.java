165f7ddf056d729105a70a8abf1c00f9621f417e
grappom <grappom@wwu.edu>
2019-02-13 13:10:19 -0800

    Update Scene.java

--- src/Scene.java
+++ src/Scene.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+
 public class Scene {
+	
+	private ArrayList<Role> leads;
+	private String name;
+	private String desc;
+	private int budget;
+	
+	public Scene(String name, String desc, ArrayList<Role> roles, int budget) {
+		this.leads = roles;
+	}
+	
+	public void cashout() {
+		
+	}
+	
+	public ArrayList<Role> getLeads() {
+		return this.leads;
+	}
+	
+	public int getBudget() {
+		return this.budget;
+	}
+	
+	public String getDesc() {
+		return this.desc;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
 
 }
-
