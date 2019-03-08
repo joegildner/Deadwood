5c64254c0863171598a4bcc2ae2e7f4501a24f6f
gildnej <gildnej@wwu.edu>
2019-03-07 16:34:05 -0800

    restructured for the logic of Model-View-Controller

---
+++ PlayDeadwood.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by gildnej on 3/7/19.
+ */
+public class PlayDeadwood {
+}
---
+++ src/PlayDeadwood.java
@@ -0,0 +1,5 @@
+public class PlayDeadwood {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
